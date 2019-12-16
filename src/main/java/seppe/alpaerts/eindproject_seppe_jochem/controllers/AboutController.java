56dba402492656111f8a2e9ded5bc0a4a4402cb8
mobapp09 <jochem_petit@msn.com>
2019-12-16 16:47:41 +0100

    fixed errors in index, problem was capitalletter in "templates"
    added nav menu
    added about page+controller
    added drop menu WIP
    added shop items to homepage

---
+++ src/main/java/seppe/alpaerts/eindproject_seppe_jochem/controllers/AboutController.java
@@ -0,0 +1,17 @@
+package seppe.alpaerts.eindproject_seppe_jochem.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import seppe.alpaerts.eindproject_seppe_jochem.model.ProductDAO;
+
+@Controller
+public class AboutController {
+    @Autowired
+    ProductDAO dao;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/about")
+    public String showAbout(){
+        return "about";
+    }
+}
