b3686c0a916f3b27066ac6713217a247f9f7d13c
mobapp11 <mobapp11@student.ehb.be>
2019-12-17 11:10:56 +0100

    Class and DAO Bestelling Controller Bestelling

--- src/main/java/seppe/alpaerts/eindproject_seppe_jochem/controllers/WinkelmandController.java
+++ src/main/java/seppe/alpaerts/eindproject_seppe_jochem/controllers/WinkelmandController.java
@@ -3,16 +3,38 @@ package seppe.alpaerts.eindproject_seppe_jochem.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import seppe.alpaerts.eindproject_seppe_jochem.model.Bestelling;
 import seppe.alpaerts.eindproject_seppe_jochem.model.BestellingDAO;
+import seppe.alpaerts.eindproject_seppe_jochem.model.DierenProduct;
+import seppe.alpaerts.eindproject_seppe_jochem.model.ProductDAO;
+
+import javax.validation.Valid;
 
 @Controller
 public class WinkelmandController {
 
     @Autowired
-    BestellingDAO dao;
+    ProductDAO dao;
+    BestellingDAO daoBestelling;
+
+    @ModelAttribute(value = "all")
+    public Iterable<DierenProduct> getAllProducten() {
+        return dao.findAll();
+    }
+
+    @ModelAttribute(value = "allBestellingen")
+    public Iterable<Bestelling> getAllBestellingen() {
+        return daoBestelling.findAll();
+    }
+
+    @ModelAttribute(value = "nBestelling")
+    public Bestelling newBestelling (){
+        return new Bestelling();
+    }
 
     @ModelAttribute
     @RequestMapping(value = {"/bestellingen"}, method = RequestMethod.GET)
@@ -20,6 +42,16 @@ public class WinkelmandController {
         return "bestellingen";
     }
 
+    @RequestMapping(value = {"/bestellingen"}, method = RequestMethod.POST)
+    public String saveBestelling(@ModelAttribute("nBestelling")@Valid Bestelling nBestelling, BindingResult bindingResult){
+        if (bindingResult.hasErrors())
+            return "bestellingen";
+        daoBestelling.save(nBestelling);
+        return "redirect:/index";}
 
 
-}
+
+
+
+
+    }
