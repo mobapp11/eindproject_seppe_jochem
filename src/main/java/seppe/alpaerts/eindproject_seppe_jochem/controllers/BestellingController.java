a8fe43aa79a987502ff5cd570c3ff70314566c4b
mobapp11 <mobapp11@student.ehb.be>
2019-12-17 14:31:19 +0100

    Class and DAO Bestelling Controller Bestelling

--- src/main/java/seppe/alpaerts/eindproject_seppe_jochem/controllers/BestellingController.java
+++ src/main/java/seppe/alpaerts/eindproject_seppe_jochem/controllers/BestellingController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +16,7 @@ import seppe.alpaerts.eindproject_seppe_jochem.model.ProductDAO;
 import javax.validation.Valid;
 
 @Controller
-public class WinkelmandController {
+public class BestellingController {
 
     @Autowired
     ProductDAO dao;
@@ -31,6 +32,7 @@ public class WinkelmandController {
         return new Bestelling();
     }
 
+
     @ModelAttribute
     @RequestMapping(value = {"/bestellingen"}, method = RequestMethod.GET)
     public String showWinkelmand(ModelMap map) {
@@ -41,7 +43,6 @@ public class WinkelmandController {
     public String saveBestelling(@ModelAttribute("nBestelling") @Valid Bestelling nBestelling, BindingResult bindingResult)
     {if(bindingResult.hasErrors())
         return "bestellingen";
-    daoBestelling.save(nBestelling);
     return "redirect:/index";}
 
 
