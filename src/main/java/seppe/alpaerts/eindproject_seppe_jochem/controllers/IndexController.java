69a2913569af2b2924e07810a6cffb946ddf8df0
mobapp09 <jochem_petit@msn.com>
2019-12-19 14:50:35 +0100

    filtering fixed

--- src/main/java/seppe/alpaerts/eindproject_seppe_jochem/controllers/IndexController.java
+++ src/main/java/seppe/alpaerts/eindproject_seppe_jochem/controllers/IndexController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.thymeleaf.model.IModel;
 import seppe.alpaerts.eindproject_seppe_jochem.model.DierenProduct;
 import seppe.alpaerts.eindproject_seppe_jochem.model.ProductDAO;
 
@@ -30,5 +32,7 @@ public class IndexController {
     public String showIndex(ModelMap map) {
         return "index";
     }
+
+
 
 }
--- src/main/java/seppe/alpaerts/eindproject_seppe_jochem/model/ProductDAO.java
+++ src/main/java/seppe/alpaerts/eindproject_seppe_jochem/model/ProductDAO.java
@@ -1,13 +1,11 @@
 package seppe.alpaerts.eindproject_seppe_jochem.model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductDAO extends CrudRepository<DierenProduct, Integer> {
 
-    public static DierenProduct findByFilter(String categorie) {
-        DierenProduct prodduct = findByFilter(categorie);
-        return prodduct;
-    }
+    Iterable<DierenProduct>  findByCategorie(String categorie) ;
 }
 
 
